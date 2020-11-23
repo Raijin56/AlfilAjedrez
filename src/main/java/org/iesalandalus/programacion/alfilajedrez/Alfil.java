8defc7818f1d1e525c31d7715c40c1e5dadbb676
Alberto <raijin56@gmail.com>
2020-11-23 21:37:32 +0100

    12.- Crea los métodos get y set para cada atributo con la visibilidad adecuada. Estos métodos siempre comprobarán la validez de los parámetros pasados y si no son correctos deberá lanzar la excepción adecuada (NullPointerException o IllegalArgumentException) con el mensaje adecuado.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -12,4 +12,26 @@ public class Alfil {
 	private Color color;
 	private Posicion posicion;
 	
+	private void setPosicion(Posicion posicion) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar una posición nula.");
+		}
+		this.posicion = posicion;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
 }
