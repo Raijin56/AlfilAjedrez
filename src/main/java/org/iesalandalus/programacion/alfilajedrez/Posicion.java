533e47fd87fecf126f9e452904f06cd0436c4d39
Alberto <raijin56@gmail.com>
2020-11-23 21:13:45 +0100

    6.- Crea los métodos get y set para los atributos. Recuerda que para el método set se debe tener en cuenta que los valores son correctos (las filas van del 1 al 8 -ambos inclusive- y las columnas de la 'a' a la 'h' -ambas inclusive-) y si no se lance una excepción del tipo IllegalArgumentException con el mensaje adecuado y no modifique los atributos.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -8,8 +8,37 @@ package org.iesalandalus.programacion.alfilajedrez;
  *
  */
 public class Posicion {
-	
+
 	private int fila;
 	private char columna;
-	
+
+	private static final int MIN_FILA = 1;
+	private static final int MAX_FILA = 8;
+	private static final char MIN_COLUMNA = 'a';
+	private static final char MAX_COLUMNA = 'h';
+
+	private void setFila(int fila) {
+		if (fila < MIN_FILA || fila > MAX_FILA) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		} else {
+			this.fila = fila;
+		}
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setColumna(char columna) {
+		if (columna < MIN_COLUMNA || columna > MAX_COLUMNA) {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		} else {
+			this.columna = columna;
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
 }
