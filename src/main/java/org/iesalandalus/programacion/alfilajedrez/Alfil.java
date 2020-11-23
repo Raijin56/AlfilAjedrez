7ea56a5bf6f70f7c40ce89129850fd51cf84e81d
Alberto <raijin56@gmail.com>
2020-11-23 21:29:50 +0100

    11.- Crea la clase Alfil, dentro del paquete adecuado, cuyos atributos serán un color (del tipo enumerado Color) y posicion (de la clase Posicion), con la visibilidad adecuada.

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.iesalandalus.programacion.alfilajedrez;
+
+/**
+ * @author Alberto
+ *
+ */
+public class Alfil {
+	
+	private Color color;
+	private Posicion posicion;
+	
+}
