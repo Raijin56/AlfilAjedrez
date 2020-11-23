02a24dc9461bf9788e8fa11431247e8bb6b8ecf5
Alberto <raijin56@gmail.com>
2020-11-23 20:56:25 +0100

    4.- Crea un enumerado llamado Direccion, dentro del paquete adecuado, que contenga los valores: ARRIBA_DERECHA, ABAJO_DERECHA, ABAJO_IZQUIERDA y ARRIBA_IZQUIERDA.

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Direccion.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.iesalandalus.programacion.alfilajedrez;
+
+/**
+ * @author raijin
+ *
+ */
+public enum Direccion {
+	ARRIBA_DERECHA, ABAJO_DERECHA, ABAJO_IZQUIERDA, ARRIBA_IZQUIERDA;
+}
